9e4a412d4a7a7cd494516ca106f07763ecc62776
Sheldon <0xd34d10cc@gmail.com>
2018-06-16 21:39:28 +0000

    Fix yuv -> bgra conversion bug (h264 returns image with padding)

--- src/main.cpp
+++ src/main.cpp
@@ -31,16 +31,125 @@ struct FrameUpdate {
   shar::Image m_image;
 };
 
+
+template<typename T>
+const T& clamp(const T& v, const T& lo, const T& hi) {
+  return v > hi ? hi :
+         v < lo ? lo :
+         v;
+}
+
+std::unique_ptr<uint8_t[]> yuv420p_to_bgra(const uint8_t* ys,
+                                           const uint8_t* us,
+                                           const uint8_t* vs,
+                                           size_t height, size_t width) {
+  size_t u_width = width / 2;
+  size_t i = 0;
+
+  auto bgra = std::make_unique<uint8_t[]>(height * width * 4);
+
+  for (int line = 0; line < height; ++line) {
+    for (int coll = 0; coll < width; ++coll) {
+
+      uint8_t y = ys[line * width + coll];
+      uint8_t u = us[(line / 2) * u_width + (coll / 2)];
+      uint8_t v = vs[(line / 2) * u_width + (coll / 2)];
+
+      int c = y - 16;
+      int d = u - 128;
+      int e = v - 128;
+
+      uint8_t r = static_cast<uint8_t>(clamp((298 * c + 409 * e + 128) >> 8, 0, 255));
+      uint8_t g = static_cast<uint8_t>(clamp((298 * c - 100 * d - 208 * e + 128) >> 8, 0, 255));
+      uint8_t b = static_cast<uint8_t>(clamp((298 * c + 516 * d + 128) >> 8, 0, 255));
+
+      bgra[i + 0] = b;
+      bgra[i + 1] = g;
+      bgra[i + 2] = r;
+      bgra[i + 3] = 0;
+
+      i += 4;
+    }
+  }
+
+  return bgra;
+}
+
+
+using ChannelData = std::vector<uint8_t>;
+
+std::array<ChannelData, 3> bgra_to_yuv420p(const shar::Image& image) {
+  std::array<ChannelData, 3> channels;
+  ChannelData& ys = channels[0];
+  ChannelData& us = channels[1];
+  ChannelData& vs = channels[2];
+
+  ys.reserve(image.size());
+  us.reserve(image.size() / 4);
+  vs.reserve(image.size() / 4);
+
+  const auto luma = [](uint8_t r, uint8_t g, uint8_t b) {
+    return static_cast<uint8_t >(((66 * r + 129 * g + 25 * b) >> 8) + 16);
+  };
+
+  const uint8_t* raw_image = image.bytes();
+  size_t i = 0;
+
+  for (auto line = 0; line < image.height(); ++line) {
+    if (line % 2 == 0) {
+      for (auto x = 0; x < image.width(); x += 2) {
+        uint8_t r = raw_image[4 * i + 2];
+        uint8_t g = raw_image[4 * i + 1];
+        uint8_t b = raw_image[4 * i];
+
+        uint8_t y = luma(r, g, b);
+        uint8_t u = static_cast<uint8_t >(((-38 * r + -74 * g + 112 * b) >> 8) + 128);
+        uint8_t v = static_cast<uint8_t >(((112 * r + -94 * g + -18 * b) >> 8) + 128);
+
+        ys.push_back(y);
+        us.push_back(u);
+        vs.push_back(v);
+
+        i++;
+
+        r = raw_image[4 * i + 2];
+        g = raw_image[4 * i + 1];
+        b = raw_image[4 * i];
+
+        y = luma(r, g, b);
+        ys.push_back(y);
+        i++;
+      }
+    }
+    else {
+      for (size_t x = 0; x < image.width(); x += 1) {
+        uint8_t r = raw_image[4 * i + 2];
+        uint8_t g = raw_image[4 * i + 1];
+        uint8_t b = raw_image[4 * i];
+
+        uint8_t y = luma(r, g, b);
+        ys.push_back(y);
+        i++;
+      }
+    }
+  }
+
+  return channels;
+}
+
+
+
 using FramePipeline = shar::FixedSizeQueue<FrameUpdate, 120>;
 
 struct FrameProvider {
   FrameProvider(FramePipeline& pipeline)
       : m_pipeline(pipeline)
-        , m_timer(std::chrono::seconds(1))
-        , m_ups(0)
-        , m_encoder(20, 1920, 1080, 5000000)
-        , m_decoder()
-        {}
+      , m_timer(std::chrono::seconds(1))
+      , m_ups(0)
+      , m_bps(0)
+      , m_fps(0)
+      , m_encoder(20, 1920, 1080, 5000000)
+      , m_decoder() {}
 
   FrameProvider(const FrameProvider&) = delete;
 
@@ -49,26 +158,37 @@ struct FrameProvider {
   void operator()(const Image& image, const Monitor&) {
     if (m_timer.expired()) {
       std::cout << "ups: " << m_ups << std::endl;
+      std::cout << "bps: " << m_bps << std::endl;
+      std::cout << "fps: " << m_fps << std::endl;
+
       m_ups = 0;
+      m_bps = 0;
+      m_fps = 0;
       m_timer.restart();
     }
     ++m_ups;
 
     shar::Image img {};
     img = image;
+
     auto packets = m_encoder.encode(img);
     for (const auto& packet: packets) {
+      m_bps += packet.size();
+
       auto decoded_frame = m_decoder.decode(packet);
       if (!decoded_frame.empty()) {
-        m_pipeline.push(FrameUpdate{0, 0, std::move(decoded_frame)});
+        m_fps += 1;
+        m_pipeline.push(FrameUpdate {0, 0, std::move(decoded_frame)});
       }
     }
-   
+
   }
 
   FramePipeline& m_pipeline;
-  shar::Timer m_timer;
-  std::size_t m_ups;
+  shar::Timer   m_timer;
+  std::size_t   m_ups;
+  std::size_t   m_bps;
+  std::size_t   m_fps;
   shar::Encoder m_encoder;
   shar::Decoder m_decoder;
 };
@@ -95,7 +215,7 @@ static void event_loop(shar::Window& window, FramePipeline& pipeline) {
       // FIXME: this loop can apply updates from different frames
       do {
         FrameUpdate* frame_update = pipeline.get_next();
-        texture.update(frame_update->x_offset,        frame_update->y_offset,
+        texture.update(frame_update->x_offset, frame_update->y_offset,
                        frame_update->m_image.width(), frame_update->m_image.height(),
                        frame_update->m_image.bytes());
         pipeline.consume(1);
@@ -139,7 +259,7 @@ int main() {
   std::size_t height  = static_cast<std::size_t>(monitor.Height);
   std::cout << "Capturing " << monitor.Name << " " << width << 'x' << height << std::endl;
 
-  auto window = create_window(width, height);
+  auto               window = create_window(width, height);
   shar::PacketSender sender;
   // auto network_thread = std::thread([&]{
   //  sender.run();
