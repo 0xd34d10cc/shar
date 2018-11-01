e504032d75622ed1ef61302938086d7e5447286d
0xd34d10cc <0xd34d10cc@gmail.com>
2018-11-01 22:46:29 +0000

    Rewrite PacketSender in Go (#58)

---
+++ client/src/processors/packet_forwarder.cpp
@@ -0,0 +1,115 @@
+#include "processors/packet_forwarder.hpp"
+#include "network/consts.hpp"
+
+
+namespace shar {
+
+PacketForwarder::PacketForwarder(Context context, IpAddress ip, Receiver<Packet> input)
+    : Base(std::move(context), std::move(input))
+    , m_ip(std::move(ip))
+    , m_context()
+    , m_socket(m_context)
+    , m_current_packet()
+    , m_state(State::SendingLength)
+    , m_length()
+    , m_bytes_sent(0)
+    {}
+
+void PacketForwarder::process(Packet packet) {
+    reset_state(std::move(packet));
+    m_context.reset();
+
+    send();
+    m_context.run();
+}
+
+void PacketForwarder::setup() {
+    Endpoint endpoint {m_ip, SERVER_DEFAULT_PORT};
+    m_socket.connect(endpoint);
+}
+
+void PacketForwarder::teardown() {
+    m_socket.shutdown(boost::asio::socket_base::shutdown_both);
+    m_socket.close();
+}
+
+void PacketForwarder::reset_state(Packet packet) {
+    m_current_packet = std::move(packet);
+    m_state = State::SendingLength;
+
+    const auto size = m_current_packet.size();
+    m_length = {
+        static_cast<std::uint8_t>((size >> 0) & 0xff),
+        static_cast<std::uint8_t>((size >> 8) & 0xff),
+        static_cast<std::uint8_t>((size >> 16) & 0xff),
+        static_cast<std::uint8_t>((size >> 24) & 0xff)
+    };
+    m_bytes_sent = 0;
+}
+
+void PacketForwarder::send() {
+    switch (m_state) {
+        case State::SendingLength: {
+            auto buffer = boost::asio::buffer(
+                m_length.data() + m_bytes_sent,
+                m_length.size() - m_bytes_sent
+            );
+
+            m_socket.async_send(buffer, [this] (const ErrorCode& ec, std::size_t bytes_sent) {
+                if (ec) {
+                    m_logger.error("Failed to send packet length: {}", ec.message());
+                    stop();
+                    return;
+                }
+
+                if (bytes_sent == 0) {
+                    m_logger.error("Unexpected EOF while sending length");
+                    stop();
+                    return;
+                }
+
+                m_bytes_sent += bytes_sent;
+                if (m_bytes_sent >= m_length.size()) {
+                    m_bytes_sent = 0;
+                    m_state = State::SendingContent;
+                }
+
+                send();
+            });
+        }
+        break;
+
+        case State::SendingContent: {
+            auto buffer = boost::asio::buffer(
+                m_current_packet.data() + m_bytes_sent,
+                m_current_packet.size() - m_bytes_sent
+            );
+
+            m_socket.async_send(buffer, [this](const ErrorCode& ec, std::size_t bytes_sent) {
+                if (ec) {
+                    m_logger.error("Failed to send packet content: {}", ec.message());
+                    stop();
+                    return;
+                }
+
+
+                if (bytes_sent == 0) {
+                    m_logger.error("Unexpected EOF while sending content");
+                    stop();
+                    return;
+                }
+
+                m_bytes_sent += bytes_sent;
+                if (m_bytes_sent >= m_current_packet.size()) {
+                    reset_state(shar::Packet());
+                    return;
+                }
+
+                send();
+            });
+        }
+        break;
+    }
+}
+
+}
---
+++ server/main.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"bufio"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+const packetsQueueSize = 30
+
+// Packet - shar network packet
+type Packet struct {
+	Data []byte
+}
+
+func (packet *Packet) readFrom(reader io.Reader) error {
+	var packetSize uint32
+
+	err := binary.Read(reader, binary.LittleEndian, &packetSize)
+	if err != nil {
+		return err
+	}
+
+	if packetSize > 0xffffff {
+		e := fmt.Sprintf("Invalid packet size %x", packetSize)
+		return errors.New(e)
+	}
+
+	packet.Data = make([]byte, packetSize)
+
+	_, err = io.ReadFull(reader, packet.Data)
+	return err
+}
+
+func (packet *Packet) writeTo(writer io.Writer) error {
+	packetSize := uint32(len(packet.Data))
+	err := binary.Write(writer, binary.LittleEndian, &packetSize)
+	if err != nil {
+		return err
+	}
+
+	_, err = writer.Write(packet.Data)
+	return err
+}
+
+func handleReceiverClient(conn net.Conn, packetsChannel chan Packet) error {
+	reader := bufio.NewReaderSize(conn, 4096)
+
+	defer func() {
+		log.Printf("Client %v disconnected", conn.RemoteAddr())
+		conn.Close()
+	}()
+
+	packet := Packet{}
+	for {
+		err := packet.readFrom(reader)
+		if err != nil {
+			log.Printf("Failed to read packet: %v", err)
+			return err
+		}
+
+		packetsChannel <- packet
+	}
+}
+
+func receivePackets(packetsChannel chan Packet) {
+	addr := ":1337"
+	listener, err := net.Listen("tcp", ":1337")
+	if err != nil {
+		log.Fatalf("[receiver] Listener creation failed: %v", err)
+	}
+
+	log.Printf("[receiver] Started on %v", addr)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalf("[receiver] Accept() failed: %v", err)
+		}
+
+		log.Printf("[receiver] Receiving packets from %v", conn.RemoteAddr())
+		err = handleReceiverClient(conn, packetsChannel)
+		log.Printf("[receiver] Finished receiving packets from %v: %v", conn.RemoteAddr(), err)
+	}
+}
+
+func runAcceptor(listeners chan net.Conn) {
+	addr := ":1338"
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("[sender] Listener creation failed: %v", err)
+	}
+
+	log.Printf("[sender] Started on %v", addr)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalf("[sender] Accept() failed: %v", err)
+		}
+
+		listeners <- conn
+	}
+}
+
+// SenderClient - sends packets to clients
+type SenderClient struct {
+	packetsChannel chan Packet
+	conn           net.Conn
+	id             int
+}
+
+func (client *SenderClient) run(finished chan int) error {
+	writer := bufio.NewWriter(client.conn)
+
+	defer func() {
+		log.Printf("[sender] Client %v [%v] disconnected", client.conn.RemoteAddr(), client.id)
+		client.conn.Close()
+		finished <- client.id
+		close(client.packetsChannel)
+	}()
+
+	for {
+		packet := <-client.packetsChannel
+		err := packet.writeTo(writer)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func sendPackets(packetsChannel chan Packet) {
+	listeners := make(chan net.Conn)
+	go runAcceptor(listeners)
+
+	id := 0
+	clients := make(map[int]chan Packet)
+	closedClients := make(chan int, 1)
+
+	for {
+		select {
+		case conn := <-listeners:
+			log.Printf("[sender] Sending packets to %v [%v]", conn.RemoteAddr(), id)
+			c := make(chan Packet, packetsQueueSize)
+			clients[id] = c
+
+			client := SenderClient{
+				packetsChannel: c,
+				conn:           conn,
+				id:             id,
+			}
+
+			go client.run(closedClients)
+			id = id + 1
+
+		case clientID := <-closedClients:
+			delete(clients, clientID)
+
+		case packet := <-packetsChannel:
+			for _, client := range clients {
+				client <- packet
+			}
+		}
+	}
+}
+
+func main() {
+	packetsChannel := make(chan Packet, packetsQueueSize)
+	go receivePackets(packetsChannel)
+	sendPackets(packetsChannel)
+}
