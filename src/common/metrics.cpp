e5bdedb01dba871d69fbe4670be4c9f1b0377ba8
tataRinaz <44062002+tataRinaz@users.noreply.github.com>
2018-11-30 12:02:37 +0400

    Delete human_readable_bytes

--- src/common/metrics.cpp
+++ src/common/metrics.cpp
@@ -3,46 +3,11 @@
 #include <cassert>
 #include <array>
 
-
-namespace {
-
-std::tuple<std::size_t, std::size_t, const char*> human_readable_bytes(std::size_t value) {
-  static const std::array<const char*, 6> suffixes = {
-      "bytes",
-      "kb",
-      "mb",
-      "gb",
-      "tb",
-      "pb"
-  };
-
-
-  std::size_t i   = 0;
-  std::size_t rem = 0;
-
-  static const std::size_t MAX  = 1u << 10u;
-  static const std::size_t MASK = MAX - 1;
-
-  while (value >= MAX) {
-    rem = value & MASK;
-    value >>= 10u;
-    ++i;
-  }
-
-  const char* suffix = i < suffixes.size() ? suffixes[i] : "too much for you";
-  const auto fraction = static_cast<std::size_t>((rem / 1024.0) * 10.0);
-  return {value, fraction, suffix};
-};
-
-
-}
-
 namespace shar {
 
 Metrics::Metrics(Logger logger)
     : m_logger(std::move(logger))
     , m_registry(std::make_shared<prometheus::Registry>()){
-
 }
 
 
