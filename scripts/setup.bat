03da95cfd4bb2d698f73359e4e035d1b8cf24bb5
0xd34d10cc <0xd34d10cc@gmail.com>
2019-02-23 23:15:45 +0000

    Update setup script: remove conanbuildinfo checks and add .git check

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,6 +1,11 @@
 @echo off
 rem Run this script from root of repo to setup build dir for VS
 
+if not exist .git (
+  echo Run from root of repo
+  exit 1
+)
+
 conan remote add --force d34dpkgs https://api.bintray.com/conan/0xd34d10cc/d34dpkgs
 conan remote add --force bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 
@@ -12,22 +17,14 @@ if not exist build\debug (
   mkdir build\debug
 )
 
-if not exist build\debug\conanbuildinfo.cmake (
-  pushd build\debug
-  conan install ..\.. --build=missing -s build_type=Debug
-  popd
-) else (
-  echo Debug: conan build info already configured
-)
+pushd build\debug
+conan install ..\.. --build=missing -s build_type=Debug
+popd
 
 if not exist build\release (
   mkdir build\release
 )
 
-if not exist build\release\conanbuildinfo.cmake (
-  pushd build\release
-  conan install ..\.. --build=missing -s build_type=Release
-  popd
-) else (
-  echo Release: conan build info already configured
-)
+pushd build\release
+conan install ..\.. --build=missing -s build_type=Release
+popd
