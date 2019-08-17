256c2fa729d66db24d347b1f353c3a9cfaa6a9a8
0xd34d10cc <0xd34d10cc@gmail.com>
2019-08-17 19:41:01 +0000

    Enable /W4 for msvc builds. Fix all warnings. (#167)

---
+++ modules/platform.cmake
@@ -0,0 +1,4 @@
+if (WIN32)
+    # shar targets win 10 only
+    set(PLATFORM_OPTIONS "/D_WIN32_WINNT=0x0A00")
+endif()
--- src/common/disable_warnings_push.hpp
+++ src/common/disable_warnings_push.hpp
@@ -7,4 +7,5 @@
 
 #if _MSC_VER && !__INTEL_COMPILER
 #pragma warning( push, 0 )
+#pragma warning( disable : 4701)
 #endif
