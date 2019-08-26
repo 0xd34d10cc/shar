d16bbdde523473ceb2f3754ce278c795b6e9a1bd
0xd34d10cc <0xd34d10cc@gmail.com>
2019-08-26 18:19:50 +0000

    Implement UTs for rtp::Depacketizer

---
+++ src/net/rtp/tests/depacketizer.cpp
@@ -0,0 +1,59 @@
+#include <iterator> // std::size
+#include <array>
+
+#include "disable_warnings_push.hpp"
+#include <gtest/gtest.h>
+#include "disable_warnings_pop.hpp"
+
+#include "net/rtp/packetizer.hpp"
+#include "net/rtp/depacketizer.hpp"
+
+
+using namespace shar::net;
+
+TEST(depacketizer, small_units) {
+  const std::uint8_t NAL_UNIT[] = {
+    0x00, 0x00, 0x01, 0x09, 0x10,
+    0x00, 0x00, 0x01, 0x67, 0x42, 0x00, 0x20, 0xe9, 0x00, 0x80, 0x0c, 0x32,
+    0x00, 0x00, 0x01, 0x68, 0xce, 0x3c, 0x80
+    // 0x00, 0x00, 0x00, 0x01, 0x65, 0x88, 0x80, 0x1a,
+  };
+
+  std::array<std::uint8_t, 1024> buffer;
+  std::memcpy(buffer.data(), NAL_UNIT, std::size(NAL_UNIT));
+
+  rtp::Packetizer packetizer{ 1100 };
+  packetizer.set(buffer.data(), std::size(NAL_UNIT));
+
+  rtp::Depacketizer depacketizer;
+  while (auto fragment = packetizer.next()) {
+    depacketizer.push(fragment);
+  }
+
+  std::size_t len = std::min(depacketizer.buffer().size(), std::size(NAL_UNIT));
+  for (std::size_t i = 0; i < len; ++i) {
+    EXPECT_EQ(NAL_UNIT[i], depacketizer.buffer()[i + 1]); /* + 1 for long prefix */
+  }
+}
+
+TEST(depacketizer, big_units) {
+  std::uint8_t NAL_UNIT[] = {
+    0x00, 0x00, 0x01, 0x67, 0x42, 0x00, 0x20, 0xe9, 0x00, 0x80, 0x0c, 0x32,
+  };
+
+  std::array<std::uint8_t, 1024> buffer;
+  std::memcpy(buffer.data(), NAL_UNIT, std::size(NAL_UNIT));
+
+  rtp::Packetizer packetizer{ 1100 };
+  packetizer.set(buffer.data(), std::size(NAL_UNIT));
+
+  rtp::Depacketizer depacketizer;
+  while (auto fragment = packetizer.next()) {
+    depacketizer.push(fragment);
+  }
+
+  std::size_t len = std::min(depacketizer.buffer().size(), std::size(NAL_UNIT));
+  for (std::size_t i = 0; i < len; ++i) {
+    EXPECT_EQ(NAL_UNIT[i], depacketizer.buffer()[i + 1]); /* + 1 for long prefix */
+  }
+}
