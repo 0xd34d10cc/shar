64295a9ea75b3c7a92cdea06ce9ee015ea2f9d72
0xd34d10cc <0xd34d10cc@gmail.com>
2019-09-05 11:43:38 +0000

    Extract dns resolution to separate function (#201)

--- src/net/receiver_factory.cpp
+++ src/net/receiver_factory.cpp
@@ -1,6 +1,7 @@
 #include "receiver_factory.hpp"
 
 #include "net/types.hpp"
+#include "net/dns.hpp"
 #include "tcp/receiver.hpp"
 #include "rtp/receiver.hpp"
 
@@ -8,30 +9,22 @@
 namespace shar::net {
 
 std::unique_ptr<IPacketReceiver> create_receiver(Context context, Url url) {
-  udp::Endpoint endpoint;
-  IOContext ioc;
-  udp::Resolver resolver{ ioc };
-  const auto port = std::to_string(url.port());
-  for (auto entry : resolver.resolve(udp::v4(), url.host(), port)) {
-    if (entry.endpoint().address().is_v4()) {
-      endpoint = entry;
-      break;
-    }
+  auto address = dns::resolve(url.host(), url.port());
+  if (auto e = address.err()) {
+    throw std::runtime_error("Failed to resolve " + url.host() + ": " + e.message());
   }
 
-  context.m_logger.info("Resolved {}:{} to {}:{}",
-                        url.host(), url.port(),
-                        endpoint.address().to_string(), endpoint.port());
-
+  context.m_logger.info("Resolved {} to {}",
+                        url.host(), address->to_string());
 
   switch (url.protocol()) {
     case Protocol::TCP:
       return std::make_unique<tcp::PacketReceiver>(std::move(context),
-                                                   endpoint.address(),
+                                                   *address,
                                                    url.port());
     case Protocol::RTP:
       return std::make_unique<rtp::Receiver>(std::move(context),
-                                             endpoint.address(),
+                                             *address,
                                              url.port());
     default:
       assert(false);
