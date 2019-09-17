7ae4c52fff5426ace8abb1f4e62e589ef9400cc1
tataRinaz <44062002+tataRinaz@users.noreply.github.com>
2019-09-18 00:37:23 +0400

    Implemented OPTION response

--- src/net/rtsp/response.hpp
+++ src/net/rtsp/response.hpp
@@ -20,7 +20,6 @@ struct Response {
   explicit Response(Headers headers);
 
   std::optional<std::size_t> parse(const char* buffer, std::size_t size);
-  //return std::nullopt if we haven't enough space in buffer
   std::optional<std::size_t> serialize(char* destination, std::size_t size);
 
   std::optional<std::size_t> m_version;
