e50ce89e51a8da2d46bad894607055544eef2cd2
Akhmetzyanov Rinaz <44062002+tataRinaz@users.noreply.github.com>
2019-09-27 01:01:14 +0400

    RTSP OPTIONS and DESCRIBE requests (#222)

--- src/net/rtsp/response.hpp
+++ src/net/rtsp/response.hpp
@@ -5,22 +5,31 @@
 
 #include "parser.hpp"
 
-
 namespace shar::net::rtsp {
 
+//Response = Status - Line; 
+//* (general - header; 
+//  | response - header; 
+//  | entity - header);
+//  CRLF
+//  [message - body]; 
+
+//Status - Line = RTSP - Version SP Status - Code SP Reason - Phrase CRLF
 struct Response {
 
   explicit Response(Headers headers);
 
   std::optional<std::size_t> parse(const char* buffer, std::size_t size);
-  //return false if we haven't enough space in buffer
-  bool serialize(char* destination, std::size_t size);
+  //return std::nullopt if we haven't enough space in buffer
+  std::optional<std::size_t> serialize(char* destination, std::size_t size);
 
   std::optional<std::size_t> m_version;
   std::uint16_t  m_status_code;
-  std::optional<std::string_view> m_reason;
+  std::optional<std::string_view> m_reason; // Reason-Phrase 
 
   Headers m_headers;
+
+  std::optional<std::string_view> m_body;
 };
 
 }
--- src/net/rtsp/server.cpp
+++ src/net/rtsp/server.cpp
@@ -1,5 +1,7 @@
 #include "server.hpp"
 
+#include <charconv>
+
 
 namespace shar::net::rtsp {
 
@@ -50,7 +52,9 @@ void Server::start_accepting(){
       assert(false);
       m_logger.error("Client {} wasn't added into server's map", client_id);
     }
+
     m_current_socket = tcp::Socket(m_context);
+
     start_accepting();
     });
 }
@@ -62,6 +66,8 @@ Server::Client::Client(tcp::Socket&& socket)
   , m_received_bytes(0)
   , m_sent_bytes(0)
   , m_headers(10)
+  , m_response_size(0)
+  , m_headers_info_buffer(100)
 {
 }
 
@@ -81,21 +87,22 @@ void Server::receive_request(ClientPos client_pos) {
     return;
   }
   auto buffer = span(client.m_buffer.data() + client.m_received_bytes, 4096 - client.m_received_bytes);
-  client.m_socket.async_receive(buffer, [this, id](const ErrorCode& ec, const std::size_t size)   {
+  client.m_socket.async_receive(buffer, [this, id](const ErrorCode& ec, const std::size_t size) {
     auto client_pos = m_clients.find(id);
 
     if (client_pos == m_clients.end()) {
       m_logger.info("Client {} not found", id);
       return;
-    } 
+    }
     if (ec) {
-      m_logger.info("Socket read error (Client {}): {}", id, ec.message());
+      m_logger.error("Socket read error (Client {}): {}", id, ec.message());
       disconnect_client(client_pos);
       return;
     }
 
-    auto& client = client_pos->second;
     try {
+
+      auto& client = client_pos->second;
       Request request({ client.m_headers.data(), client.m_headers.size() });
       client.m_received_bytes += size;
       auto parsing_result = request.parse(reinterpret_cast<const char*>(client.m_buffer.data()), client.m_received_bytes);
@@ -104,10 +111,20 @@ void Server::receive_request(ClientPos client_pos) {
         return;
       }
       client.m_sent_bytes = 0;
-      send_response(client_pos);
+      auto response = proccess_request(client_pos, request);
+
+      if (auto response_size = 
+        response.serialize(reinterpret_cast<char*>(client.m_buffer.data()),
+                                    client.m_buffer.size())) {
+        client.m_response_size = response_size.value();
+        send_response(client_pos);
+        return;
+      }
+      m_logger.error("Buffer overflow at the response serializing. ClientId {}", id);
+      disconnect_client(client_pos);
     }
     catch (const std::runtime_error& error) {
-      m_logger.warning("Client {} request parsing error: {}", id, error.what());  
+      m_logger.info("Client {} request parsing error: {}", id, error.what());
       disconnect_client(client_pos);
     }
     });
@@ -115,33 +132,107 @@ void Server::receive_request(ClientPos client_pos) {
 }
 
 void Server::send_response(ClientPos client_pos) {
-  static std::string_view simple_response =
-    "RTSP/1.0 501 NOT_IMPLEMENTED\r\n"
-    "\r\n";
 
   auto& client = client_pos->second;
   auto id = client_pos->first;
 
-  auto buffer = span(simple_response.data() + client.m_sent_bytes, simple_response.size() - client.m_sent_bytes);
+
+  auto buffer = span(client.m_buffer.data() + client.m_sent_bytes, client.m_response_size - client.m_sent_bytes);
   client.m_socket.async_send(buffer, [this, id](const ErrorCode& ec, const std::size_t size) {
     auto client_pos = m_clients.find(id);
-
     if (client_pos == m_clients.end()) {
       m_logger.info("Client {} not found", id);
       return;
     }
     if (ec) {
-      m_logger.info("Socket send error (Client {}", id);
+      m_logger.error("Socket send error (Client {}): {}", id, ec.message());
       disconnect_client(client_pos);
       return;
     }
-
     auto& client = client_pos->second;
     client.m_sent_bytes += size;
-    if (simple_response.size() == client.m_sent_bytes) {
+    if (client.m_response_size == client.m_sent_bytes) {
       client.m_received_bytes = 0;
       receive_request(client_pos);
     }
     });
 }
+
+
+Response Server::proccess_request(ClientPos client_pos, Request request) {
+  assert(request.m_type.has_value());
+  Response response({client_pos->second.m_headers.data(), client_pos->second.m_headers.size()});
+  auto  cseq_pos = std::find_if(request.m_headers.data, request.m_headers.data + request.m_headers.len,
+    [](const Header& header) {
+      return header.key == "CSeq";
+    });
+
+  if (cseq_pos == request.m_headers.data + request.m_headers.len) {
+    m_logger.warning("CSeq header wasn't found. Client {}", client_pos->first);
+    response.m_version = 1;
+    response.m_status_code = 400;
+    response.m_reason = "Bad request";
+
+    response.m_headers.len = 0;
+    return response;
+  }
+
+  switch (request.m_type.value()) {
+  case Request::Type::OPTIONS: {
+    response.m_version = 1;
+    response.m_status_code = 200;
+    response.m_reason = "OK";
+    response.m_headers.data[0] = *cseq_pos;
+    response.m_headers.data[1] = Header("Public", "DESCRIBE");
+    response.m_headers.len = 2;
+    return response;
+  }
+  case Request::Type::DESCRIBE: {
+    static std::string_view simple_sdp = "o=- 1815849 0 IN IP4 127.0.0.1\r\n"
+      "c = IN IP4 127.0.0.1\r\n"
+      "m = video 1336 RTP / AVP 96\r\n"
+      "a = rtpmap:96 H264 / 90000\r\n"
+      "a = fmtp : 96 packetization - mode = 1";
+    auto& buffer = client_pos->second.m_headers_info_buffer;
+
+    auto [num_end_ptr, ec] = std::to_chars(buffer.data(), buffer.data()+buffer.size(), simple_sdp.size());
+    if (ec != std::errc()) {
+      m_logger.error("Cannot convert SDP size from number to string");
+      assert(false);
+    }
+    response.m_version = 1;
+    response.m_status_code = 200;
+    response.m_reason = "0K";
+    response.m_headers.data[0] = *cseq_pos;
+    response.m_headers.data[1] = Header("Content-type", "application/sdp");
+    response.m_headers.data[2] = Header("Contrent-length", 
+                                        std::string_view(buffer.data(), 
+                                                         num_end_ptr + 1 - buffer.data()));
+    response.m_headers.len = 3;
+    response.m_body = simple_sdp;
+    return response;
+  }
+  case Request::Type::SETUP:
+  case Request::Type::TEARDOWN:
+  case Request::Type::PLAY:
+  case Request::Type::PAUSE:
+  case Request::Type::GET_PARAMETER:
+  case Request::Type::SET_PARAMETER:
+  case Request::Type::REDIRECT:
+  case Request::Type::ANNOUNCE:
+  case Request::Type::RECORD: {
+    response.m_version = 1;
+    response.m_status_code = 501;
+    response.m_reason = "Not implemented";
+    response.m_headers.len = 0;
+    return response;
+  }
+  default: {
+    assert(false);  
+    m_logger.error("Unknown request type. {}", static_cast<int>(request.m_type.value()));
+    throw std::runtime_error("Unknown request type.");
+  }
+  }
+}
+
 }
