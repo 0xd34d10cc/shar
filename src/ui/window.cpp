afbbfce1160bb2873f7f135787e22cd23b7db362
0xd34d10cc <0xd34d10cc@gmail.com>
2020-06-07 12:46:04 +0000

    Request debug OpenGL context only in debug builds

--- src/ui/window.cpp
+++ src/ui/window.cpp
@@ -88,7 +88,9 @@ static Window::GLContextPtr init_gl(SDL_Window* window) {
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+#ifdef SHAR_DEBUG_BUILD
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+#endif SHAR_DEBUG_BUILD
 
   SDL_GL_SetSwapInterval(1);
 
