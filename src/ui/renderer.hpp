30767ceb9229a54a9e432f04ecedc024f9ea9d1c
0xd34d10cc <0xd34d10cc@gmail.com>
2020-06-07 10:29:30 +0000

    Render video frames via modern OpenGL API (glDrawElements)

--- src/ui/renderer.hpp
+++ src/ui/renderer.hpp
@@ -22,6 +22,28 @@ class State;
 class Window;
 class Texture;
 
+using AttributeID = i32;
+
+struct ShaderAttributes {
+  AttributeID position{ 0 }; 
+  AttributeID texture_coordinates{ 0 };
+  AttributeID color{ 0 };
+
+  AttributeID projection{ 0 };
+  AttributeID texture{ 0 };
+};
+
+using ProgramID = u32;
+
+struct Shader {
+  ProgramID vertex{ 0 };
+  ProgramID fragment{ 0 };
+  ProgramID program{ 0 };
+
+  ShaderAttributes attributes;
+};
+
+
 class Renderer {
 public:
   static void init_log(const Logger& logger);
@@ -32,16 +54,24 @@ public:
   // render and clear state
   // TODO: decouple command list from State
   void render(State& state, const Window& window);
-  void render(Texture& texture, Size window_size,
-              Point at, Size texture_size);
+  void render(Texture& texture, const Window& window,
+              Point at, Size texture_size, usize y_offset);
 
   nk_user_font* default_font_handle() const;
+
+  // compile shader from sources
+  Shader compile(const char* vertex_shader, const char* fragment_shader);
+  
+  // free shader and associated resources
+  void free(Shader& shader);
 
 private:
   void init();
   void destroy();
 
   OpenGLVTable m_gl;
+  Shader m_shader;
+  
   std::unique_ptr<SDLDevice> m_device;
   std::unique_ptr<nk_font_atlas> m_atlas;
 };
