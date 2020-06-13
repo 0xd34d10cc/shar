9658b257002bd71d2855a6a9657cec7c93d136a5
0xd34d10cc <0xd34d10cc@gmail.com>
2020-06-13 12:11:52 +0000

    Remove redundant runner class

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,11 +1,23 @@
-#include "runner.hpp"
+#include "config.hpp"
+#include "app.hpp"
+#include "ui/controls/message_box.hpp"
 
 #include "disable_warnings_push.hpp"
+#include <fmt/format.h>
 #include <SDL2/SDL_main.h>
 #include "disable_warnings_pop.hpp"
 
 
 int main(int argc, char* argv[]) {
-  shar::Runner app_runner;
-  return app_runner.run(argc, argv);
+  try {
+    auto config = shar::Config::from_args(argc, argv);
+    shar::App app{std::move(config)};
+    return app.run();
+  } catch (const std::exception& e) {
+    const auto message = fmt::format("Unhandled exception: {}", e.what());
+    auto error =
+        shar::ui::MessageBox::error("Fatal error", message);
+    error.show();
+    return EXIT_FAILURE;
+  }
 }
