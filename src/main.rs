f608f2f26298615b069e500d0b512a10f274a40e
0xd34d10cc <0xd34d10cc@gmail.com>
2021-04-25 22:55:21 +0000

    Store configuration in json file

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,24 @@ use iced::{Application, Settings};
 
 mod capture;
 mod codec;
+mod config;
 mod net;
 mod resolve;
 mod stream;
 mod ui;
 mod view;
 
+use config::Config;
 use ui::App;
 
 fn main() -> Result<()> {
     dotenv::dotenv().ok();
     env_logger::init();
-    App::run(Settings::with_flags(()))?;
+    let config = config::load().unwrap_or_else(|e| {
+        log::error!("Failed to read configuration file: {}", e);
+        Config::default()
+    });
+
+    App::run(Settings::with_flags(config))?;
     Ok(())
 }
